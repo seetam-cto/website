4d90a840a572f2e7c36247181f10f46f79f7c7a2
seetam-cto <seetam@creativejoule.in>
2022-11-17 18:58:29 +0530

    ss

--- pages/property/[id].jsx
+++ pages/property/[id].jsx
@@ -337,8 +337,12 @@ const PropertyDetails = ({property}) => {
 
 export async function getStaticPaths() {
     return {
-      paths: [{ params: { id: '63752b4aad3e8758ae6ad042' } }],
-      fallback: false, // can also be true or 'blocking'
+      paths: [
+        { params: { id: '63752b4aad3e8758ae6ad042' } }, 
+        { params: { id: '6375b543d18eb430546c20a6' } }, 
+        { params: { id: '6375bab8d18eb430546c2111' } }, 
+        { params: { id: '6375bd93d18eb430546c215c' } }],
+      fallback: true, // can also be true or 'blocking'
     }
   }
 
